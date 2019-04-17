107e5612ae1f676f653f2b2cb97d10afeae61a9b
kristinchow <kchow@seas.upenn.edu>
2019-04-17 15:45:31 -0400

    Promises work to find events yay

--- routes/calendar.js
+++ routes/calendar.js
@@ -2,6 +2,43 @@ var express = require('express')
 var router = express.Router();
 var isAuthenticated = require('../middlewares/isAuthenticated')
 var User = require('../models/user.js')
+var Event = require('../models/event.js')
+
+
+var checkIfEventToday = function(events, event, date, callback) {
+    var today = date;
+    Event.findById(event, function (err, event) {
+        if (!err) {
+            if (event.date.getUTCDate() === today.getDate() &&
+                event.date.getUTCFullYear() === today.getFullYear() &&
+                event.date.getUTCMonth() === today.getMonth()) {
+                events.push(event);
+            }
+            callback(events);
+        }
+    })
+}
+3
+var findEvents = function(req, date, callback) {
+    var count = 0;
+    var events = [];
+    User.findOne({username: req.session.user}, function (err, result) {
+        if (!err) {
+            for (let i = 0; i < result.events.length; i++) {
+                checkIfEventToday(events, result.events[i], date, function(e) {
+                    events = e;
+                    count++;
+                    if (count === result.events.length) {
+                        callback(null, events);
+                    }
+                })
+
+            }
+        } else {
+            callback(null, [])
+        }
+    })
+}
 
 router.get('/monthly', function (req, res) {
     var date = new Date();
@@ -11,31 +48,75 @@ router.get('/monthly', function (req, res) {
     var it = 0;
 
     var weeks = [];
-    console.log(max)
     var f = firstDay.getDay();
 
     while (it != max) {
-        var weekObj = {
-            days: []
-        };
+        var week = [];
         for (let i = 0; i < 7; i++) {
             if (i < f || i > f) {
-                weekObj.days.push(0);
+                var day = {
+                    num: 0,
+                    events: []
+                }
+                week.push(day);
             } else {
                 f++;
                 it++;
-                weekObj.days.push(it);
+                var day = {
+                    num: it,
+                    events: []
+                }
+                week.push(day);
                 if (it === max) {
                     f = 0;
                 }
             }
         }
         f = 0;
-        weeks.push(weekObj);
+        weeks.push(week);
     }
-    res.render('monthly', {
-        user: req.session.user,
-        weeks: weeks
+
+    var promises = [];
+
+    for (let i = 0; i < weeks.length; i++) {
+        var w = weeks[i];
+        for (let j = 0; j < w.length; j++) {
+            var d = w[j];
+            if (d.num != 0) {
+                //find events
+                var day = new Date(date.getFullYear(), date.getMonth(), d.num);
+                var p = new Promise(function(resolve, reject) {
+                    findEvents(req, day, function(err, e) {
+                        resolve(e);
+                    });
+                })
+                promises.push(p);
+            }
+        }
+    }
+
+    Promise.all(promises).then(function(values) {
+        var count = 0;
+        for (let i = 0; i < weeks.length; i++) {
+            var w = weeks[i];
+            for (let j = 0; j < w.length; j++) {
+                var d = w[j];
+                if (d.num != 0) {
+                    d.events = values[count];
+                    count++;
+                }
+            }
+        }
+
+        console.log(weeks)
+
+        res.render('monthly', {
+            user: req.session.user,
+            weeks: weeks
+        })
     })
+
+
+
 })
 module.exports = router;
